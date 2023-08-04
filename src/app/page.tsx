dbb86dc7aaeb6a16dcbe112aa68cdd22d95cea89
guven8 <guvenkemal86@gmail.com>
2023-08-04 15:15:56 +0100

    move components folder out of /app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import EyebrowsServices from './components/eyebrowsServices';
-import HeroSection from './components/heroSection';
-import MakeUpServices from './components/makeUpServices';
-import NailsServices from './components/nailsServices';
+import EyebrowsServices from '@/components/eyebrowsServices';
+import HeroSection from '@/components/heroSection';
+import MakeUpServices from '@/components/makeUpServices';
+import NailsServices from '@/components/nailsServices';
 
 export default function Home() {
   return (
