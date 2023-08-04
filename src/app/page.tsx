8971e00594860be19bd5e33e30a96d3d978035ec
guven8 <guvenkemal86@gmail.com>
2023-08-04 12:34:49 +0100

    fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import EyebrowsServices from './components/eyebrowsServices';
 import HeroSection from './components/heroSection';
-import MakeUpServices from './components/makeupServices';
+import MakeUpServices from './components/makeUpServices';
 import NailsServices from './components/nailsServices';
 
 export default function Home() {
